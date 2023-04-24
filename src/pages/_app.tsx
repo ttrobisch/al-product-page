c501143105cc7559926a48c081a8802e3ac4cd51
jbcker <jona.boecker@web.de>
2023-04-24 16:01:49 +0200

    add icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="description" content={data.page_description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
       </Head>
       <div className={inter.className + " mx-auto"}>
         <Component {...pageProps} />
