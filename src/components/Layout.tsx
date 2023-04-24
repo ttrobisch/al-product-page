8e9c26564b4aa3730cb7137883fc0692aa65142c
jbcker <jona.boecker@web.de>
2023-04-24 16:14:21 +0200

    removed old icon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children, ...rest }: Props) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/icons/android-chrome-192x192.png" />
         <meta name="theme-color" content="#fff" />
       </Head>
       <main {...rest}>{children}</main>
