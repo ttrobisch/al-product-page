192943b683a1867772faaafca42637ed7f9beee1
Tobias Trobisch <tobias.trobisch@gmail.com>
2023-04-28 14:06:57 +0200

    optimized images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="9J6nGIlysRaNgfcAWpyCnc3KT5cjTQqH72I0xGU-W78" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="manifest" href="/site.webmanifest" />
