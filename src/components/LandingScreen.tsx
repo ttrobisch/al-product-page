8bb7bdbb2c0e7d763a36601f264a93e53261b5e0
Tobias Trobisch <tobias.trobisch@gmail.com>
2023-05-02 12:57:26 +0200

    fix locale highlight

--- src/components/LandingScreen.tsx
+++ src/components/LandingScreen.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import RocketIcon from "@heroicons/react/20/solid/RocketLaunchIcon";
 import LinkIcon from "@heroicons/react/20/solid/LinkIcon";
 import DownIcon from "@heroicons/react/20/solid/ArrowDownCircleIcon";
+import { useRouter } from "next/router";
 
 type Props = {
   bulletpoints: { text: string }[];
@@ -17,7 +18,7 @@ type Props = {
 
 function LandingScreen(props: Props) {
   const [showScroll, setShowScroll] = React.useState(true);
-  const [currentLang, setCurrentLang] = React.useState("");
+  const currentLang = useRouter().locale;
 
   React.useEffect(() => {
     const offset = 20;
@@ -33,23 +34,45 @@ function LandingScreen(props: Props) {
       window.removeEventListener("scroll", checkScrollTop);
     };
   }, []);
-  React.useEffect(() => {
-    setCurrentLang(window.location.pathname.split("/")[1]);
-  }, []);
   return (
     <>
       <div
         className="box-content grid place-items-center
-       justify-items-start gap-4 rounded-xl bg-frontscreen pl-8 lg:pl-32 pt-6 lg:pt-8 lg:grid-cols-[minmax(0,1fr)_minmax(600px,1fr)]"
+       justify-items-start gap-4 rounded-xl bg-frontscreen pl-8 pt-6 lg:grid-cols-[minmax(0,1fr)_minmax(600px,1fr)] lg:pl-32 lg:pt-8"
       >
         <h1 className="text-4xl font-extrabold text-white md:text-6xl lg:text-8xl">
           Autonomous <br /> Logistics
         </h1>
-        <div className="font-extrabold text-white text-l absolute top-5 right-6">
-          <a href="/de">DE</a> | <a href="/en">EN</a>
+        <div className={"text-l absolute right-6 top-5 text-white"}>
+          <a
+            className={`no-underline hover:underline ${
+              currentLang === "de" ? "font-extrabold" : ""
+            }`}
+            href="/de"
+          >
+            DE{" "}
+          </a>
+          |
+          <a
+            className={`no-underline hover:underline ${
+              currentLang === "en" || currentLang === "" ? "font-extrabold" : ""
+            }`}
+            href="/en"
+          >
+            {" "}
+            EN
+          </a>{" "}
         </div>
         <div className="justify-self-center md:row-span-3 lg:col-start-2 lg:justify-self-start">
-          <Image src={props.amr_url} alt="Ein Bild vom Husky" width={600} height={400} quality={75} placeholder="empty" priority />
+          <Image
+            src={props.amr_url}
+            alt="Ein Bild vom Husky"
+            width={600}
+            height={400}
+            quality={75}
+            placeholder="empty"
+            priority
+          />
         </div>
         <div className="text-sm font-extralight md:text-base lg:self-end">
           <ul className="grid gap-3">
@@ -59,11 +82,19 @@ function LandingScreen(props: Props) {
           </ul>
         </div>
         <div className="grid w-max grid-cols-2 place-items-start gap-4 pb-4">
-          <a href={props.trial_kit_url} className="grid grid-cols-[1fr_auto] items-center justify-items-start gap-3 rounded bg-[#96B0B3] px-3 py-2 shadow hover:bg-[#E80381] hover:text-white ">
+          <a
+            href={props.trial_kit_url}
+            className="grid grid-cols-[1fr_auto] items-center justify-items-start gap-3 rounded bg-[#96B0B3] px-3 py-2 shadow hover:bg-[#E80381] hover:text-white "
+          >
             <RocketIcon className="inline w-4" />
             {props.trial_kit_label}
           </a>
-          <a className="grid grid-cols-[1fr_auto] items-center justify-items-start gap-3 rounded bg-[#96B0B3] px-3 py-2 shadow hover:bg-[#E80381] hover:text-white" href={`mailto:${props.mail_address}?subject=${encodeURIComponent(props.mail_subject)}&body=${encodeURIComponent(props.mail_body)}`}>
+          <a
+            className="grid grid-cols-[1fr_auto] items-center justify-items-start gap-3 rounded bg-[#96B0B3] px-3 py-2 shadow hover:bg-[#E80381] hover:text-white"
+            href={`mailto:${props.mail_address}?subject=${encodeURIComponent(
+              props.mail_subject
+            )}&body=${encodeURIComponent(props.mail_body)}`}
+          >
             <LinkIcon className="inline w-4" />
             {props.contact_label}
           </a>
@@ -71,7 +102,9 @@ function LandingScreen(props: Props) {
         {showScroll && (
           <button
             onClick={() => {
-              document.getElementById("amrs")?.scrollIntoView({ behavior: "smooth" });
+              document
+                .getElementById("amrs")
+                ?.scrollIntoView({ behavior: "smooth" });
             }}
             className="fixed bottom-0 right-0 mx-auto pb-5 pr-6  text-black text-opacity-30"
           >
