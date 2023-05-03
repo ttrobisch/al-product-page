29890439edbf6ca410b25baca9d5348e3d918dc0
jbcker <jona.boecker@web.de>
2023-05-03 14:33:10 +0200

    style AMRCard

--- src/components/AMRFacts.stories.tsx
+++ src/components/AMRFacts.stories.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
 import {AMRFacts} from "./AMRFacts";
+import "../lib/styles.css";
 export const World = () => <AMRFacts drive_type={"Vierbeinig"} charging_speed={"1 h"} max_speed={"1,6 m/s"} payload={"14 kg"} weight={"32,7 kg"}/>;
--- src/components/AMRFacts.tsx
+++ src/components/AMRFacts.tsx
@@ -1,41 +1,34 @@
 import React from "react";
-import "tailwindcss/tailwind.css";
 
 type Props = {
-  max_speed: string;
-  weight: string;
-  drive_type: string;
-  charging_speed: string;
-  payload: string;
+    max_speed: string;
+    weight: string;
+    drive_type: string;
+    charging_speed: string;
+    payload: string;
 };
 
 function AMRFacts(props: Props) {
-  return<>
-    <div className="flex flex-col ml-10 mt-16 mb-16 sm:flex-row justify-start">
-        <h2 className="uppercase text-5xl font-extrabold text-gray-200 rotate-0 sm:-rotate-90 mb-6 sm:mb-0">facts</h2>
-      <div className={"flex flex-col space-y-3"}>
-        <div className={"flex flex-row justify-between gap-8"}>
-          <p className={"font-bold text-right"}>Höchstgeschwindigkeit</p>
-          <p className={"text-left"}>{props.max_speed}</p>
+    return <>
+        <div className="grid grid-cols-1 md:grid-cols-[auto_1fr] md:gap-12">
+            <h2
+                className="uppercase vertical-text text-5xl font-extrabold text-gray-200 mb-8 md:mb-0 md:text-center">facts</h2>
+            <div className="grid items-center">
+                <div className={"grid min-[320px]:grid-cols-[auto_1fr] gap-x-8 min-[320px]:gap-y-4"}>
+                    <p className={"font-bold min-[320px]:text-right"}>Höchstgeschwindigkeit</p>
+                    <p className={"text-left"}>{props.max_speed}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>Gewicht</p>
+                    <p className={"text-left"}>{props.weight}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>Antriebsart</p>
+                    <p className={"text-left"}>{props.drive_type}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>Ladegeschwindigkeit</p>
+                    <p className={"text-left"}>{props.charging_speed}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>Traglast</p>
+                    <p className={"text-left"}>{props.payload}</p>
+                </div>
+            </div>
         </div>
-        <div className={"flex flex-row justify-between"}>
-          <p className={"font-bold text-right"}>Gewicht</p>
-          <p className={"text-left"}>{props.weight}</p>
-        </div>
-        <div className={"flex flex-row justify-between"}>
-          <p className={"font-bold text-right"}>Antriebsart</p>
-          <p className={"text-left"}>{props.drive_type}</p>
-        </div>
-        <div className={"flex flex-row justify-between"}>
-          <p className={"font-bold text-right"}>Ladegeschwindigkeit</p>
-          <p className={"text-left"}>{props.charging_speed}</p>
-        </div>
-        <div className={"flex flex-row justify-between"}>
-          <p className={"font-bold text-right"}>Traglast</p>
-          <p className={"text-left"}>{props.payload}</p>
-        </div>
-      </div>
-    </div>
     </>
 }
-export{AMRFacts}
+
+export {AMRFacts}
