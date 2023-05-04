cd14ced7775aecf2feb20a72bf2ff68300aea440
jbcker <jona.boecker@web.de>
2023-05-04 11:01:34 +0200

    implement lbls to amrFacts Card

--- src/components/AMRFacts.stories.tsx
+++ src/components/AMRFacts.stories.tsx
@@ -1,4 +1,22 @@
 import React from 'react';
 import {AMRFacts} from "./AMRFacts";
 import "../lib/styles.css";
-export const World = () => <AMRFacts drive_type={"Vierbeinig"} charging_speed={"1 h"} max_speed={"1,6 m/s"} payload={"14 kg"} weight={"32,7 kg"}/>;
+
+let facts = {
+    max_speed: "1,6 m/s",
+    weight: "32,7 kg",
+    drive_type: "Vierbeinig",
+    charging_speed: "1 h",
+    payload: "14 kg"
+}
+let labels = {
+    headline_facts: "facts",
+    headline_usage: "usage",
+    max_speed_lbl: "Höchstgeschwindigkeit",
+    weight_lbl: "Gewicht",
+    drive_type_lbl: "Antriebsart",
+    charging_speed_lbl: "Ladegeschwindigkeit",
+    payload_lbl: "Traglast"
+}
+
+export const World = () => <AMRFacts facts={facts} labels={labels} />;
--- src/components/AMRFacts.tsx
+++ src/components/AMRFacts.tsx
@@ -1,30 +1,41 @@
 import React from "react";
 
 type Props = {
-    max_speed: string;
-    weight: string;
-    drive_type: string;
-    charging_speed: string;
-    payload: string;
+    facts: {
+        max_speed: string;
+        weight: string;
+        drive_type: string;
+        charging_speed: string;
+        payload: string;
+      };
+    labels: {
+        headline_facts: string;
+        headline_usage: string;
+        max_speed_lbl: string;
+        weight_lbl: string;
+        drive_type_lbl: string;
+        charging_speed_lbl: string;
+        payload_lbl: string;
+    };
 };
 
-function AMRFacts(props: Props) {
+function AMRFacts (props: Props) {
     return <>
         <div className="grid grid-cols-1 md:grid-cols-[auto_1fr] md:gap-12">
             <h2
                 className="uppercase vertical-text text-5xl font-extrabold text-gray-200 mb-8 md:mb-0 md:text-center">facts</h2>
             <div className="grid items-center">
                 <div className={"grid min-[320px]:grid-cols-[auto_1fr] gap-x-8 min-[320px]:gap-y-4"}>
-                    <p className={"font-bold min-[320px]:text-right"}>Höchstgeschwindigkeit</p>
-                    <p className={"text-left"}>{props.max_speed}</p>
-                    <p className={"font-bold min-[320px]:text-right"}>Gewicht</p>
-                    <p className={"text-left"}>{props.weight}</p>
-                    <p className={"font-bold min-[320px]:text-right"}>Antriebsart</p>
-                    <p className={"text-left"}>{props.drive_type}</p>
-                    <p className={"font-bold min-[320px]:text-right"}>Ladegeschwindigkeit</p>
-                    <p className={"text-left"}>{props.charging_speed}</p>
-                    <p className={"font-bold min-[320px]:text-right"}>Traglast</p>
-                    <p className={"text-left"}>{props.payload}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>{props.labels.max_speed_lbl}</p>
+                    <p className={"text-left"}>{props.facts.max_speed}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>{props.labels.weight_lbl}</p>
+                    <p className={"text-left"}>{props.facts.weight}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>{props.labels.drive_type_lbl}</p>
+                    <p className={"text-left"}>{props.facts.drive_type}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>{props.labels.charging_speed_lbl}</p>
+                    <p className={"text-left"}>{props.facts.charging_speed}</p>
+                    <p className={"font-bold min-[320px]:text-right"}>{props.labels.payload_lbl}</p>
+                    <p className={"text-left"}>{props.facts.payload}</p>
                 </div>
             </div>
         </div>
