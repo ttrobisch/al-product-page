48e73b1f6276bf5abe3cf4bd2d85f309d0c0c355
belanagawolek <belana.gawolek@telekom.de>
2023-05-12 15:02:58 +0200

    layout landingScreen

--- src/components/LandingScreen.tsx
+++ src/components/LandingScreen.tsx
@@ -38,41 +38,23 @@ function LandingScreen(props: Props) {
     <>
       <div
         className="box-content grid place-items-center
-       justify-items-start gap-4 rounded-xl bg-frontscreen pl-8 pt-6 lg:grid-cols-[minmax(0,1fr)_minmax(600px,1fr)] lg:pl-32 lg:pt-8"
+       justify-items-start gap-4 overflow-hidden rounded-xl bg-frontscreen pl-8 pt-6 lg:grid-cols-[minmax(0,1fr)_minmax(600px,1fr)] lg:pl-32 lg:pt-8"
       >
         <h1 className="text-4xl font-extrabold text-white md:text-6xl lg:text-8xl">
           Autonomous <br /> Logistics
         </h1>
         <div className={"text-l absolute right-6 top-5 text-white"}>
-          <a
-            className={`no-underline hover:underline ${
-              currentLang === "de" ? "font-extrabold" : ""
-            }`}
-            href="/de"
-          >
+          <a className={`no-underline hover:underline ${currentLang === "de" ? "font-extrabold" : ""}`} href="/de">
             DE{" "}
           </a>
           |
-          <a
-            className={`no-underline hover:underline ${
-              currentLang === "en" || currentLang === "" ? "font-extrabold" : ""
-            }`}
-            href="/en"
-          >
+          <a className={`no-underline hover:underline ${currentLang === "en" || currentLang === "" ? "font-extrabold" : ""}`} href="/en">
             {" "}
             EN
           </a>{" "}
         </div>
-        <div className="justify-self-center md:row-span-3 lg:col-start-2 lg:justify-self-start">
-          <Image
-            src={props.amr_url}
-            alt="Ein Bild vom Husky"
-            width={600}
-            height={400}
-            quality={75}
-            placeholder="empty"
-            priority
-          />
+        <div className="justify-self-center  md:row-span-3 lg:col-start-2 lg:justify-self-start">
+          <Image className=" translate-x-[2.5rem] sm:translate-x-[5.5rem] " src={props.amr_url} alt="Ein Bild vom Husky" width={700} height={400} quality={75} placeholder="empty" priority />
         </div>
         <div className="text-sm font-extralight md:text-base lg:self-end">
           <ul className="grid gap-3">
@@ -82,19 +64,11 @@ function LandingScreen(props: Props) {
           </ul>
         </div>
         <div className="grid w-max grid-cols-2 place-items-start gap-4 pb-4">
-          <a
-            href={props.trial_kit_url}
-            className="grid grid-cols-[1fr_auto] items-center justify-items-start gap-3 rounded bg-[#96B0B3] px-3 py-2 shadow hover:bg-[#E80381] hover:text-white "
-          >
+          <a href={props.trial_kit_url} className="grid grid-cols-[1fr_auto] items-center justify-items-start gap-3 rounded bg-[#96B0B3] px-3 py-2 shadow hover:bg-[#E80381] hover:text-white ">
             <RocketIcon className="inline w-4" />
             {props.trial_kit_label}
           </a>
-          <a
-            className="grid grid-cols-[1fr_auto] items-center justify-items-start gap-3 rounded bg-[#96B0B3] px-3 py-2 shadow hover:bg-[#E80381] hover:text-white"
-            href={`mailto:${props.mail_address}?subject=${encodeURIComponent(
-              props.mail_subject
-            )}&body=${encodeURIComponent(props.mail_body)}`}
-          >
+          <a className="grid grid-cols-[1fr_auto] items-center justify-items-start gap-3 rounded bg-[#96B0B3] px-3 py-2 shadow hover:bg-[#E80381] hover:text-white" href={`mailto:${props.mail_address}?subject=${encodeURIComponent(props.mail_subject)}&body=${encodeURIComponent(props.mail_body)}`}>
             <LinkIcon className="inline w-4" />
             {props.contact_label}
           </a>
@@ -102,9 +76,7 @@ function LandingScreen(props: Props) {
         {showScroll && (
           <button
             onClick={() => {
-              document
-                .getElementById("amrs")
-                ?.scrollIntoView({ behavior: "smooth" });
+              document.getElementById("amrs")?.scrollIntoView({ behavior: "smooth" });
             }}
             className="fixed bottom-0 right-0 mx-auto pb-5 pr-6  text-black text-opacity-30"
           >
