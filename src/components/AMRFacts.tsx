476644f922fa67c5619378ccfcc5179f45dc9de1
belanagawolek <belana.gawolek@telekom.de>
2023-05-17 12:19:53 +0200

    centered facts block

--- src/components/AMRFacts.tsx
+++ src/components/AMRFacts.tsx
@@ -24,39 +24,25 @@ type Props = {
 function AMRFacts({ labels, facts }: Props) {
   return (
     <>
-      <div className="ml-10  grid grid-cols-1 md:grid-cols-[auto_1fr] md:gap-12">
-        <div className="grid md:justify-items-start md:ml-10">
-          <h2 className="vertical-text mb-8 text-5xl font-extrabold uppercase text-gray-200 md:mb-0 md:text-center md:text-8xl">
-            facts
-          </h2>
+      <div className="ml-4 grid grid-cols-1 md:gap-12 xl:relative xl:ml-10 xl:grid-cols-[auto_1fr]">
+        <div className="grid justify-items-start px-4 md:ml-10 xl:absolute">
+          <h2 className="vertical-text mb-8 text-5xl font-extrabold uppercase text-gray-200 md:mb-0 md:text-center md:text-8xl">facts</h2>
         </div>
 
-        <div className="grid md:justify-items-center md:pr-36 text-base md:text-2xl">
-          <div className="grid gap-x-8 md:gap-x-32 min-[320px]:grid-cols-[auto_1fr] min-[320px]:gap-y-4">
-            <p className="font-bold min-[320px]:text-right">
-              {labels.max_speed_lbl}
-            </p>
-            <p className={"text-left"}>{facts.max_speed}</p>
-            <p className="font-bold min-[320px]:text-right">
-              {labels.weight_lbl}
-            </p>
-            <p className={"text-left"}>{facts.weight}</p>
-            <p className="font-bold min-[320px]:text-right">
-              {labels.drive_type_lbl}
-            </p>
-            <p className={"text-left"}>{facts.drive_type}</p>
-            <p className="font-bold min-[320px]:text-right">
-              {labels.operation_time_lbl}
-            </p>
-            <p className={"text-left"}>{facts.operation_time}</p>
-            <p className="font-bold min-[320px]:text-right">
-              {labels.charging_speed_lbl}
-            </p>
-            <p className={"text-left"}>{facts.charging_speed}</p>
-            <p className="font-bold min-[320px]:text-right">
-              {labels.payload_lbl}
-            </p>
-            <p className={"text-left"}>{facts.payload}</p>
+        <div className="grid w-full justify-self-center px-4  text-base md:justify-items-center md:text-2xl ">
+          <div className="grid gap-x-5 min-[450px]:grid-cols-2 min-[450px]:gap-y-4 md:gap-x-24 ">
+            <p className="font-bold min-[450px]:text-right">{labels.max_speed_lbl}</p>
+            <p className={"mb-3 text-left min-[450px]:mb-0"}>{facts.max_speed}</p>
+            <p className="font-bold min-[450px]:text-right">{labels.weight_lbl}</p>
+            <p className={"mb-3 text-left min-[450px]:mb-0"}>{facts.weight}</p>
+            <p className="font-bold min-[450px]:text-right">{labels.drive_type_lbl}</p>
+            <p className={"mb-3 text-left min-[450px]:mb-0"}>{facts.drive_type}</p>
+            <p className="font-bold min-[450px]:text-right">{labels.operation_time_lbl}</p>
+            <p className={"mb-3 text-left min-[450px]:mb-0"}>{facts.operation_time}</p>
+            <p className="font-bold min-[450px]:text-right">{labels.charging_speed_lbl}</p>
+            <p className={"mb-3 text-left min-[450px]:mb-0"}>{facts.charging_speed}</p>
+            <p className="font-bold min-[450px]:text-right">{labels.payload_lbl}</p>
+            <p className={"mb-3 text-left min-[450px]:mb-0"}>{facts.payload}</p>
           </div>
         </div>
       </div>
@@ -64,4 +50,4 @@ function AMRFacts({ labels, facts }: Props) {
   );
 }
 
-export {AMRFacts}
+export { AMRFacts };
