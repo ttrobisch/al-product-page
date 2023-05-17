02637ccd644e2d30cbd262e398948e56ed8ed7cf
belanagawolek <belana.gawolek@telekom.de>
2023-05-17 12:55:42 +0200

    landingscreen same color with footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
     extend: {
       backgroundImage: {
         frontscreen:
-          "linear-gradient(185deg, rgb(39, 45, 54) 0vh, rgb(39, 45, 54) 55vh, rgba(151, 185, 188, 0.2) calc(55vh + 1px), rgba(151, 185, 188, 0.5) 100%)",
+          "linear-gradient(185deg, rgb(65,77,86) 0vh, rgb(39, 45, 54) 55vh, rgba(151, 185, 188, 0.2) calc(55vh + 1px), rgba(151, 185, 188, 0.5) 100%)",
         "frontscreen-big":
-          "conic-gradient(from 275deg at 62vw 54vh, rgb(39, 45, 54), rgb(151, 185, 188), transparent)",
+          "conic-gradient(from 275deg at 62vw 54vh, rgb(65,77,86), rgb(151, 185, 188), transparent)",
         line: "linear-gradient(90deg, transparent 0%, transparent 15%, rgba(0,0,0,0.2) calc(15% + 1px), rgba(0,0,0,0.2) 100%)",
       },
       backgroundColor: {
