01f66c4755cd3bd03d27935c85511ad0e954c4fc
belanagawolek <belana.gawolek@telekom.de>
2023-07-10 15:45:20 +0200

    header background animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,122 +35,139 @@ function Header(props: Props) {
 
   return (
     <LayoutGroup>
-      <motion.div
-        layout
-        className={clsx({
-          "relative grid gap-8 overflow-hidden rounded-b-3xl bg-[#D2D7D9] px-8 py-8 pb-5 transition-all duration-700 ease-in-out lg:gap-20 lg:px-24 lg:py-20": true,
-          "text-color bg-transparent": !isOpen,
-          "flex-grow": isOpen,
-        })}
-        onMouseLeave={handleAmrClose}
-      >
-        <div className="flex flex-wrap justify-between text-[#141D1E]">
-          <a className="text-left text-sm hover:font-semibold" href="">
-            Autonomous Logistics
-          </a>
-          <button className="text-right text-sm" onClick={handleClick}>
-            {isOpen ? (
-              <>
-                <CrossIcon className="mr-2 " />
-                <span className="max-lg:hidden">close</span>
-              </>
-            ) : (
-              <>
-                <BurgerMenu className="mr-2 h-[14px] " />
-                <span className="max-lg:hidden">Menu</span>
-              </>
-            )}
-          </button>
-        </div>
+      <motion.div className="relative h-full w-full">
         <AnimatePresence presenceAffectsLayout>
           {isOpen && (
-            <motion.div transition={{ duration: 0.4 }} initial={{ opacity: 0, height: 0 }} animate={{ opacity: 1, height: "auto" }} exit={{ opacity: 0, height: 0 }}>
-              <div className="grid grid-cols-[1fr_30%] gap-12 pb-12 lg:pb-24">
-                <div>
-                  <div className="flex flex-col text-3xl lg:flex-row lg:pb-4 lg:text-6xl">
-                    <a className="pr-10 font-extralight hover:font-normal" href="" onMouseEnter={handleAmrClose}>
-                      Home
-                    </a>
-                    <a className="pr-10  font-extralight hover:font-normal" href="" onMouseEnter={handleAmrClose}>
-                      Solution
-                    </a>
-                    <a
-                      className={clsx({
-                        "pr-10  font-extralight hover:font-normal": true,
-                        "font-normal": showAMRList,
-                      })}
-                      href=""
-                      onMouseEnter={() => {
-                        setAMRIndex(-1);
-                        setShowAMRList(true);
-                      }}
-                    >
-                      Modelle
-                    </a>
-                  </div>
-                  <AnimatePresence>
-                    {showAMRList && (
-                      <div
-                        className={clsx({
-                          "flex flex-col flex-wrap overflow-hidden pb-4 pl-4 transition-all duration-700 ease-in-out lg:flex-row lg:pl-0": true,
-                        })}
-                      >
-                        {props.amrs.map((amr, index) => (
-                          <motion.a layout className={clsx("inline-block pr-10 text-lg font-thin hover:font-light lg:text-2xl", {})} initial={{ opacity: 0, height: 0 }} exit={{ opacity: 0, height: 0 }} animate={{ opacity: 1, height: "auto" }} key={amr.name} onMouseEnter={() => setAMRIndex(index)}>
-                            {amr.name}
-                          </motion.a>
-                        ))}
-                      </div>
-                    )}
-                  </AnimatePresence>
-                  <div className="flex flex-col text-3xl lg:flex-row lg:text-6xl">
-                    <a className="pr-10 font-extralight hover:font-normal" href="" onMouseEnter={handleAmrClose}>
-                      Team
-                    </a>
-                    <a className="pr-10  font-extralight hover:font-normal" href="" onMouseEnter={handleAmrClose}>
-                      Blog
-                    </a>
-                  </div>
-                </div>
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: showAMRList ? 1 : 0 }}
-                  exit={{ opacity: 0 }}
-                  className={clsx({
-                    " rounded-lg bg-black p-4 max-lg:hidden ": true,
-                  })}
-                >
-                  <img className="pr-10 text-3xl font-thin hover:font-light" src={amrUrl} alt={amrAlt} />
-                </motion.div>
-              </div>
-              <div className="">
-                <nav className=" flex flex-col flex-wrap justify-between text-sm text-[#141D1E] md:flex-row ">
-                  <div className="space-x-6 text-right md:text-left">
-                    <a className="hover:font-semibold" href="">
-                      Impressum
-                    </a>
-                    <a className="hover:font-semibold" href="">
-                      Datenschutz
-                    </a>
-                    <a className="hover:font-semibold" href="">
-                      FAQ
-                    </a>
-                  </div>
-                  <div className={"order-first pb-4 text-right text-sm md:order-last "}>
-                    <a className={`no-underline hover:underline ${currentLang === "de" ? "font-extrabold" : ""}`} href="/de">
-                      DE{" "}
-                    </a>
-                    |
-                    <a className={`no-underline hover:underline ${currentLang === "en" || currentLang === "" ? "font-extrabold" : ""}`} href="/en">
-                      {" "}
-                      EN
-                    </a>{" "}
-                  </div>
-                </nav>
-              </div>
-            </motion.div>
+            <motion.div
+              layout
+              transition={{ duration: 1, delay: 0.1 }}
+              initial={{ opacity: 0, height: "0px", width: "100vw" }}
+              animate={{ opacity: 1, height: "100vh", width: "100vw" }}
+              className={clsx({
+                "duration-900 absolute px-8 py-8 pb-5 transition-opacity ": true,
+                "hidden bg-white": !isOpen,
+                " flex-grow bg-[#000000aa]": isOpen,
+              })}
+            ></motion.div>
           )}
         </AnimatePresence>
+        <motion.div
+          layout
+          className={clsx({
+            " absolute grid w-full gap-8 overflow-hidden rounded-b-3xl bg-[#D2D7D9] px-8 py-8 pb-5 transition-all duration-700 ease-in-out lg:gap-20 lg:px-24 lg:py-20": true,
+            "text-color h-auto bg-transparent": !isOpen,
+            "flex-grow": isOpen,
+          })}
+          onMouseLeave={handleAmrClose}
+        >
+          <div className="flex flex-wrap justify-between text-[#141D1E]">
+            <a className="text-left text-sm hover:font-semibold" href="">
+              Autonomous Logistics
+            </a>
+            <button className="text-right text-sm" onClick={handleClick}>
+              {isOpen ? (
+                <>
+                  <CrossIcon className="mr-2 " />
+                  <span className="max-lg:hidden">close</span>
+                </>
+              ) : (
+                <>
+                  <BurgerMenu className="mr-2 h-[14px] " />
+                  <span className="max-lg:hidden">Menu</span>
+                </>
+              )}
+            </button>
+          </div>
+          <AnimatePresence presenceAffectsLayout>
+            {isOpen && (
+              <motion.div transition={{ duration: 0.4 }} initial={{ opacity: 0, height: 0 }} animate={{ opacity: 1, height: "auto" }} exit={{ opacity: 0, height: 0 }}>
+                <div className="grid grid-cols-[1fr_30%] gap-12 pb-12 lg:pb-24">
+                  <div>
+                    <div className="flex flex-col text-3xl lg:flex-row lg:pb-4 lg:text-6xl">
+                      <a className="pr-10 font-extralight hover:font-normal" href="" onMouseEnter={handleAmrClose}>
+                        Home
+                      </a>
+                      <a className="pr-10  font-extralight hover:font-normal" href="" onMouseEnter={handleAmrClose}>
+                        Solution
+                      </a>
+                      <a
+                        className={clsx({
+                          "pr-10  font-extralight hover:font-normal": true,
+                          "font-normal": showAMRList,
+                        })}
+                        href=""
+                        onMouseEnter={() => {
+                          setAMRIndex(-1);
+                          setShowAMRList(true);
+                        }}
+                      >
+                        Modelle
+                      </a>
+                    </div>
+                    <AnimatePresence>
+                      {showAMRList && (
+                        <div
+                          className={clsx({
+                            "flex flex-col flex-wrap overflow-hidden pb-4 pl-4 transition-all duration-700 ease-in-out lg:flex-row lg:pl-0": true,
+                          })}
+                        >
+                          {props.amrs.map((amr, index) => (
+                            <motion.a layout className={clsx("inline-block pr-10 text-lg font-thin hover:font-light lg:text-2xl", {})} initial={{ opacity: 0, height: 0 }} exit={{ opacity: 0, height: 0 }} animate={{ opacity: 1, height: "auto" }} key={amr.name} onMouseEnter={() => setAMRIndex(index)}>
+                              {amr.name}
+                            </motion.a>
+                          ))}
+                        </div>
+                      )}
+                    </AnimatePresence>
+                    <div className="flex flex-col text-3xl lg:flex-row lg:text-6xl">
+                      <a className="pr-10 font-extralight hover:font-normal" href="" onMouseEnter={handleAmrClose}>
+                        Team
+                      </a>
+                      <a className="pr-10  font-extralight hover:font-normal" href="" onMouseEnter={handleAmrClose}>
+                        Blog
+                      </a>
+                    </div>
+                  </div>
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: showAMRList ? 1 : 0 }}
+                    exit={{ opacity: 0 }}
+                    className={clsx({
+                      " rounded-lg bg-black p-4 max-lg:hidden ": true,
+                    })}
+                  >
+                    <img className="pr-10 text-3xl font-thin hover:font-light" src={amrUrl} alt={amrAlt} />
+                  </motion.div>
+                </div>
+                <div className="">
+                  <nav className=" flex flex-col flex-wrap justify-between text-sm text-[#141D1E] md:flex-row ">
+                    <div className="space-x-6 text-right md:text-left">
+                      <a className="hover:font-semibold" href="">
+                        Impressum
+                      </a>
+                      <a className="hover:font-semibold" href="">
+                        Datenschutz
+                      </a>
+                      <a className="hover:font-semibold" href="">
+                        FAQ
+                      </a>
+                    </div>
+                    <div className={"order-first pb-4 text-right text-sm md:order-last "}>
+                      <a className={`no-underline hover:underline ${currentLang === "de" ? "font-extrabold" : ""}`} href="/de">
+                        DE{" "}
+                      </a>
+                      |
+                      <a className={`no-underline hover:underline ${currentLang === "en" || currentLang === "" ? "font-extrabold" : ""}`} href="/en">
+                        {" "}
+                        EN
+                      </a>{" "}
+                    </div>
+                  </nav>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </motion.div>
       </motion.div>
     </LayoutGroup>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,200 +1,217 @@
 import React from "react";
-import {Headline} from "../components/Headline";
-import {AmrCard} from "../components/AmrCard";
-import {VideoCard} from "../components/VideoCard";
-import {join} from "path";
-import {readFileSync, readdirSync} from "fs";
+import { Headline } from "../components/Headline";
+import { AmrCard } from "../components/AmrCard";
+import { VideoCard } from "../components/VideoCard";
+import { join } from "path";
+import { readFileSync, readdirSync } from "fs";
 import yaml from "js-yaml";
 import matter from "gray-matter";
-import {LandingScreen} from "../components/LandingScreen";
-import {GetStaticPropsContext} from "next";
+import { LandingScreen } from "../components/LandingScreen";
+import { GetStaticPropsContext } from "next";
 import Head from "next/head";
-import {Footer} from "../components/Footer";
-import {footer} from "mdast-util-to-hast/lib/footer";
+import { Footer } from "../components/Footer";
+import { footer } from "mdast-util-to-hast/lib/footer";
+import { Header } from "../components/Header";
 
 type Props = {
+  amrs: {
+    name: string;
+    image: string;
+  }[];
+  videos: {
+    title: string;
+    thumbnail: string;
+    url: string;
+  }[];
+  data: {
+    headline: string;
+    introduction_text: string;
+    background_image_url: string;
+    background_image_alt: string;
+
+    trial_kit_url: string;
+    trial_kit_label: string;
+    trial_kit_description: string;
+
+    contact_url: string;
+    contact_label: string;
+    contact_description: string;
+
+    mail_address: string;
+    mail_subject: string;
+    mail_body: string;
+
+    video_url: string;
+    video_title: string;
+    video_subtext: string;
+
+    legal_notice_url: string;
+    legal_notice_label: string;
+
+    page_title: string;
+    page_description: string;
+
+    amr_headline: string;
+    amr_subtext: string;
+
     amrs: {
-        name: string;
-        image: string;
+      name: string;
+      description: string;
+      image_url: string;
+      image_alt: string;
     }[];
+
     videos: {
-        title: string;
-        thumbnail: string;
-        url: string;
+      name: string;
+      thumbnail: string;
+      url: string;
     }[];
-    data: {
-        headline: string;
-        introduction_text: string;
-        background_image_url: string;
-        background_image_alt: string;
 
-        trial_kit_url: string;
-        trial_kit_label: string;
-        trial_kit_description: string;
-
-        contact_url: string;
-        contact_label: string;
-        contact_description: string;
-
-        mail_address: string;
-        mail_subject: string;
-        mail_body: string;
-
-        video_url: string;
-        video_title: string;
-        video_subtext: string;
-
-        legal_notice_url: string;
-        legal_notice_label: string;
-
-        page_title: string;
-        page_description: string;
-
-        amr_headline: string;
-        amr_subtext: string;
-
-        amrs: {
-            name: string;
-            description: string;
-            image_url: string;
-            image_alt: string;
-        }[];
-
-        videos: {
-            name: string;
-            thumbnail: string;
-            url: string;
-        }[];
-
-        logo_url: string;
-        logo_alt: string;
-        bulletpoints: { text: string }[];
-    };
-    footer: {
-        header: string;
-        contact_label: string;
-        contact_url: string;
-        data_protection_label: string;
-        data_protection_url: string;
-        legal_notice_label: string;
-        legal_notice_url: string;
-        faq_label: string;
-        faq_url: string;
-        copyright: string;
-        goto_top_label: string;
-    }
+    logo_url: string;
+    logo_alt: string;
+    bulletpoints: { text: string }[];
+  };
+  footer: {
+    header: string;
+    contact_label: string;
+    contact_url: string;
+    data_protection_label: string;
+    data_protection_url: string;
+    legal_notice_label: string;
+    legal_notice_url: string;
+    faq_label: string;
+    faq_url: string;
+    copyright: string;
+    goto_top_label: string;
+  };
 };
 
-export default function Index({data, amrs, videos, footer}: Props) {
-    return (
-        <>
-            <div className="grid gap-14 p-[5vw]">
-                <Head>
-                    <title>{data.page_title}</title>
-                    <meta name="description" content={data.page_description}/>
-                </Head>
-                <div className="-m-[5vw] p-4 mb-0 grid min-h-screen">
-                    <LandingScreen bulletpoints={data.bulletpoints} amr_url={data.background_image_url}
-                                   contact_label={data.contact_label} mail_address={data.mail_address}
-                                   mail_subject={data.mail_subject} mail_body={data.mail_body}
-                                   trial_kit_label={data.trial_kit_label} trial_kit_url={data.trial_kit_url}/>
-                </div>
+const props = {
+  amrs: [
+    { name: "Husky", img: "/images/amr_husky.webp" },
+    { name: "Shuttle", img: "/images/amr_shuttle.webp" },
+    { name: "Spot", img: "/images/amr_spot.webp" },
+    { name: "Capra", img: "/images/amr_capra.webp" },
+    { name: "Dingo", img: "/images/amr_dingo.webp" },
+    { name: "Crayler", img: "/images/amr_crayler.webp" },
+    { name: "G4T4", img: "/images/amr_g4t4.webp" },
+    { name: "Rapyuta", img: "/images/amr_rapyuta.webp" },
+    { name: "T-Bot", img: "/images/amr_tbot.webp" },
+  ],
+  fallbackImg: "/images/background_models.png",
+  fallbackImgAlt: "Models",
+  pages: ["Home", "Solution", "Models", "Unser Team", "Blog"],
+  color: "#141D1E",
+};
 
-                <div
-                    className="mx-auto max-w-screen-2xl pb-8 text-right text-lg font-medium uppercase text-transparent lg:text-3xl">
-                    <div className="mb-8 h-px bg-line"/>
-                    <div className="text-gray-400">{data.page_description}</div>
-                </div>
+export default function Index({ data, amrs, videos, footer }: Props) {
+  return (
+    <>
+      <Header {...props} />
+      <div className="grid gap-14 p-[5vw]">
+        <Head>
+          <title>{data.page_title}</title>
+          <meta name="description" content={data.page_description} />
+        </Head>
 
-                <div id="amrs" className="mx-auto max-w-screen-2xl w-full space-y-8">
-                    <Headline>{data.amr_headline}</Headline>
+        <div className="-m-[5vw] mb-0 grid min-h-screen p-4">
+          <LandingScreen bulletpoints={data.bulletpoints} amr_url={data.background_image_url} contact_label={data.contact_label} mail_address={data.mail_address} mail_subject={data.mail_subject} mail_body={data.mail_body} trial_kit_label={data.trial_kit_label} trial_kit_url={data.trial_kit_url} />
+        </div>
 
-                    <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-4">
-                        {amrs.map((amr) => (
-                            <div key={amr.name} id={"#" + amr.name.toLowerCase()} className="">
-                                <AmrCard {...amr} />
-                            </div>
-                        ))}
-                    </div>
-                </div>
+        <div className="mx-auto max-w-screen-2xl pb-8 text-right text-lg font-medium uppercase text-transparent lg:text-3xl">
+          <div className="mb-8 h-px bg-line" />
+          <div className="text-gray-400">{data.page_description}</div>
+        </div>
 
-                <div id="videos" className="mx-auto mb-24 max-w-screen-2xl w-full space-y-8">
-                    <Headline>{data.video_title}</Headline>
-                    <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-4">
-                        {videos.map((video) => (
-                            <div key={video.url}>
-                                <VideoCard {...video} />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-            <footer className="m-auto">
-                <Footer {...footer}/>
-            </footer>
-        </>
-    );
+        <div id="amrs" className="mx-auto w-full max-w-screen-2xl space-y-8">
+          <Headline>{data.amr_headline}</Headline>
+
+          <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-4">
+            {amrs.map((amr) => (
+              <div key={amr.name} id={"#" + amr.name.toLowerCase()} className="">
+                <AmrCard {...amr} />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div id="videos" className="mx-auto mb-24 w-full max-w-screen-2xl space-y-8">
+          <Headline>{data.video_title}</Headline>
+          <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-4">
+            {videos.map((video) => (
+              <div key={video.url}>
+                <VideoCard {...video} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <footer className="m-auto">
+        <Footer {...footer} />
+      </footer>
+    </>
+  );
 }
 
 export function getStaticProps(props: GetStaticPropsContext) {
-    const amr_path = join(process.cwd(), "_content/amrs_" + props.locale);
-    const amr_files = readdirSync(amr_path);
+  const amr_path = join(process.cwd(), "_content/amrs_" + props.locale);
+  const amr_files = readdirSync(amr_path);
 
-    const amrs = amr_files.map((file) => {
-        const fileContent = readFileSync(join(amr_path, file), "utf-8");
-        const matterResult = matter(fileContent, {
-            engines: {
-                yaml: (s) => yaml.load(s, {schema: yaml.JSON_SCHEMA}) as object,
-            },
-        });
-        return matterResult.data as {
-            name: string;
-            image: string;
-        };
+  const amrs = amr_files.map((file) => {
+    const fileContent = readFileSync(join(amr_path, file), "utf-8");
+    const matterResult = matter(fileContent, {
+      engines: {
+        yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+      },
     });
-
-    const videos_path = join(process.cwd(), "_content/videos_" + props.locale);
-    const videos_files = readdirSync(videos_path);
-
-    const videos = videos_files.map((file) => {
-        const fileContent = readFileSync(join(videos_path, file), "utf-8");
-        const matterResult = matter(fileContent, {
-            engines: {
-                yaml: (s) => yaml.load(s, {schema: yaml.JSON_SCHEMA}) as object,
-            },
-        });
-        return matterResult.data as {
-            title: string;
-            thumbnail: string;
-            url: string;
-        };
-    });
-
-    const data_path = join(process.cwd(), "meta/frontpage." + props.locale + ".yml");
-    const fileContent = readFileSync(data_path, "utf-8");
-    const result = matter(fileContent, {
-        engines: {
-            yaml: (s) => yaml.load(s, {schema: yaml.JSON_SCHEMA}) as object,
-        },
-    });
-    const data = result.data as Props['data'];
-
-    const footer_path = join(process.cwd(), "meta/footer." + props.locale + ".yml");
-    const footerContent = readFileSync(footer_path, "utf-8");
-    const footerresult = matter(footerContent, {
-        engines: {
-            yaml: (s) => yaml.load(s, {schema: yaml.JSON_SCHEMA}) as object,
-        },
-    });
-    const footer = footerresult.data as Props['footer'];
-    footer.contact_url = "mailto:" + data.mail_address + "?subject=" + data.mail_subject + "&body=" + data.mail_body;
-
-    return {
-        props: {
-            data,
-            amrs,
-            videos,
-            footer,
-        },
+    return matterResult.data as {
+      name: string;
+      image: string;
     };
+  });
+
+  const videos_path = join(process.cwd(), "_content/videos_" + props.locale);
+  const videos_files = readdirSync(videos_path);
+
+  const videos = videos_files.map((file) => {
+    const fileContent = readFileSync(join(videos_path, file), "utf-8");
+    const matterResult = matter(fileContent, {
+      engines: {
+        yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+      },
+    });
+    return matterResult.data as {
+      title: string;
+      thumbnail: string;
+      url: string;
+    };
+  });
+
+  const data_path = join(process.cwd(), "meta/frontpage." + props.locale + ".yml");
+  const fileContent = readFileSync(data_path, "utf-8");
+  const result = matter(fileContent, {
+    engines: {
+      yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+    },
+  });
+  const data = result.data as Props["data"];
+
+  const footer_path = join(process.cwd(), "meta/footer." + props.locale + ".yml");
+  const footerContent = readFileSync(footer_path, "utf-8");
+  const footerresult = matter(footerContent, {
+    engines: {
+      yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+    },
+  });
+  const footer = footerresult.data as Props["footer"];
+  footer.contact_url = "mailto:" + data.mail_address + "?subject=" + data.mail_subject + "&body=" + data.mail_body;
+
+  return {
+    props: {
+      data,
+      amrs,
+      videos,
+      footer,
+    },
+  };
 }
